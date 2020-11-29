09f71734a8d3c599d3fa860381bdab647fedf79c
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-11-28 21:07:40 -0300

    v1.3

--- src/db/strategies/postgres/postgres.js
+++ src/db/strategies/postgres/postgres.js
@@ -43,14 +43,25 @@ class Postgres extends ICrud {
     return this._schema.findAll({where: item, raw:true})
   }
   static async connect() {
+    const SSL_DB = process.env.SSL_DB === 'true' ? true : undefined
+    const SSL_DB_REJECT = process.env.SSL_DB_REJECT === 'false' ? false : undefined
+    
+    let dialectOptions = {}
+    if (SSL_DB) {
+        dialectOptions = {
+            ssl: {
+                require: SSL_DB,
+                rejectUnauthorized: SSL_DB_REJECT,
+            }
+        };
+    };
+
     const connection = new Sequelize(process.env.POSTGRES_URL, {
-      logging: false,
-      quoteIdentifiers: false,
-      ssl: process.env.SSL_DB,
-      dialectOptions: {
-        ssl: process.env.SSL_DB
-      }
-    })
+        quoteIdentifiers: false,
+        logging: false,
+        dialectOptions,
+    });
+    
     return connection
   }
 }
